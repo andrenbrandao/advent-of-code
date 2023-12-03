3f05b67532fca3cc5bdc62be9e827177aaea5de4
André Brandão <andrenbrandao@gmail.com>
2023-12-03 13:00:49 -0300

    feat(2023-day1): add more test values

--- 2023/day1/main_test.go
+++ 2023/day1/main_test.go
@@ -7,11 +7,11 @@ import (
 
 func TestDay1(t *testing.T) {
 	t.Run("extracts calibration values", func(t *testing.T) {
-		calibrationDocument := []string{"1abc2"}
+		calibrationDocument := []string{"1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"}
 		trebuchet := Trebuchet{calibrationDocument}
 
 		got := trebuchet.CalibrationValues()
-		want := []int{12}
+		want := []int{12, 38, 15, 77}
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
