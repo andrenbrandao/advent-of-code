ad047855779c75c5bb362b0e36b37b967642d5f9
André Brandão <andrenbrandao@gmail.com>
2023-12-03 17:00:39 -0300

    feat(2023-day1): refactor trebuchet to pkg

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -2,50 +2,10 @@ package main
 
 import (
 	"bufio"
+	"day1/pkg/trebuchet"
 	"fmt"
 	"os"
-	"strconv"
-	"unicode"
 )
-
-type Trebuchet struct {
-	calibrationDocument []string
-}
-
-func (t *Trebuchet) CalibrationValues() []int {
-	result := make([]int, len(t.calibrationDocument))
-	for i, s := range t.calibrationDocument {
-		firstInt := -1
-		lastInt := -1
-
-		for _, c := range s {
-			if !unicode.IsDigit(c) {
-				continue
-			}
-
-			if firstInt == -1 {
-				firstInt, _ = strconv.Atoi(string(c))
-			}
-
-			lastInt, _ = strconv.Atoi(string(c))
-		}
-
-		result[i] = firstInt*10 + lastInt
-	}
-
-	return result
-}
-
-func (t *Trebuchet) Sum() int {
-	calibrationValues := t.CalibrationValues()
-	sum := 0
-
-	for _, val := range calibrationValues {
-		sum += val
-	}
-
-	return sum
-}
 
 func main() {
 	file, err := os.Open("input")
@@ -62,6 +22,6 @@ func main() {
 		document = append(document, reader.Text())
 	}
 
-	trebuchet := Trebuchet{calibrationDocument: document}
+	trebuchet := trebuchet.NewTrebuchet(document)
 	fmt.Println(trebuchet.Sum())
 }
