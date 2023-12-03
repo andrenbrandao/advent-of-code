6ecd9a6564380400a50ee2a542c3fc906b1bd6a8
André Brandão <andrenbrandao@gmail.com>
2023-12-03 20:49:03 -0300

    feat(2023-day1): part2 using words as tokens

--- 2023/day1/part2/pkg/trebuchet/trebuchet.go
+++ 2023/day1/part2/pkg/trebuchet/trebuchet.go
@@ -1,10 +1,5 @@
 package trebuchet
 
-import (
-	"strconv"
-	"unicode"
-)
-
 type Trebuchet struct {
 	calibrationDocument []string
 }
@@ -15,23 +10,11 @@ func NewTrebuchet(calibrationDocument []string) *Trebuchet {
 
 func (t *Trebuchet) CalibrationValues() []int {
 	result := make([]int, len(t.calibrationDocument))
+
 	for i, s := range t.calibrationDocument {
-		firstInt := -1
-		lastInt := -1
+		value := t.extractValue(s)
 
-		for _, c := range s {
-			if !unicode.IsDigit(c) {
-				continue
-			}
-
-			if firstInt == -1 {
-				firstInt, _ = strconv.Atoi(string(c))
-			}
-
-			lastInt, _ = strconv.Atoi(string(c))
-		}
-
-		result[i] = firstInt*10 + lastInt
+		result[i] = value
 	}
 
 	return result
@@ -46,4 +29,58 @@ func (t *Trebuchet) Sum() int {
 	}
 
 	return sum
+}
+
+func (*Trebuchet) extractValue(s string) int {
+	tokenMap := map[string]int{
+		"0":     0,
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	firstInt := -1
+	lastInt := -1
+	n := len(s)
+
+	// Time Complexity: O(n^2*t*k)
+	// where n is the size of the word
+	// t is the size of the longest token
+	// k is the number of tokens
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			c := s[i : j+1]
+
+			val, ok := tokenMap[string(c)]
+			if !ok {
+				continue
+			}
+
+			i = j + 1
+
+			if firstInt == -1 {
+				firstInt = val
+			}
+
+			lastInt = val
+		}
+	}
+
+	return firstInt*10 + lastInt
 }
--- 2023/day1/part2/pkg/trebuchet/trebuchet_test.go
+++ 2023/day1/part2/pkg/trebuchet/trebuchet_test.go
@@ -7,11 +7,35 @@ import (
 
 func TestCalibrationValues(t *testing.T) {
 	t.Run("extracts calibration values", func(t *testing.T) {
-		calibrationDocument := []string{"1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"}
+		calibrationDocument := []string{
+			"1abc2",
+			"pqr3stu8vwx",
+			"a1b2c3d4e5f",
+			"treb7uchet",
+		}
 		trebuchet := Trebuchet{calibrationDocument}
 
 		got := trebuchet.CalibrationValues()
 		want := []int{12, 38, 15, 77}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("extracts words as values", func(t *testing.T) {
+		calibrationDocument := []string{
+			"two1nine",
+			"eightwothree",
+			"abcone2threexyz",
+			"xtwone3four",
+			"4nineeightseven2",
+			"zoneight234",
+			"7pqrstsixteen",
+		}
+		trebuchet := Trebuchet{calibrationDocument}
+
+		got := trebuchet.CalibrationValues()
+		want := []int{29, 83, 13, 24, 42, 14, 76}
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
