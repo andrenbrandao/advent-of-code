547ae225cc3c58fc0f13740ff703f7479c09e9c4
André Brandão <andrenbrandao@gmail.com>
2023-12-04 19:33:26 -0300

    feat(2023-day3): extract parts from schematic

--- 2023/day3/part1/pkg/engine/engine.go
+++ 2023/day3/part1/pkg/engine/engine.go
@@ -1,11 +1,41 @@
 package engine
 
-type EngineSchematic struct{}
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+type EngineSchematic struct {
+	schematic string
+}
 
 func NewEngineSchematic(input string) *EngineSchematic {
-	return nil
+	return &EngineSchematic{input}
 }
 
 func (e *EngineSchematic) Parts() []int {
-	return []int{}
+	lines := strings.Split(e.schematic, "\n")
+	var parts []int
+
+	for _, line := range lines {
+		n := len(line)
+
+		for i := 0; i < n; i++ {
+			j := i
+
+			if unicode.IsDigit(rune(line[j])) {
+				for j < n && unicode.IsDigit(rune(line[j])) {
+					j++
+				}
+
+				partInt, _ := strconv.Atoi(line[i:j])
+				parts = append(parts, partInt)
+
+				i = j
+			}
+		}
+	}
+
+	return parts
 }
