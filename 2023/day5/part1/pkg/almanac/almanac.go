5195bba447f7ed0ce716ea1f6767d11fb2c3ca82
André Brandão <andrenbrandao@gmail.com>
2023-12-06 21:38:22 -0300

    feat(2023-day5): create map

--- 2023/day5/part1/pkg/almanac/almanac.go
+++ 2023/day5/part1/pkg/almanac/almanac.go
@@ -1,1 +1,30 @@
 package almanac
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Map map[int]int
+
+func NewMap(input string) Map {
+	lines := strings.Split(input, "\n")
+	aMap := Map{}
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+
+		fields := strings.Fields(line)
+		destination, _ := strconv.Atoi(fields[0])
+		source, _ := strconv.Atoi(fields[1])
+		aRange, _ := strconv.Atoi(fields[2])
+
+		for i := 0; i < aRange; i++ {
+			aMap[source+i] = destination + i
+		}
+	}
+
+	return aMap
+}
--- 2023/day5/part1/pkg/almanac/almanac_test.go
+++ 2023/day5/part1/pkg/almanac/almanac_test.go
@@ -1,1 +1,31 @@
 package almanac
+
+import "testing"
+
+func TestMap(t *testing.T) {
+	t.Run("creates a map from an input", func(t *testing.T) {
+		input := `0 15 37
+37 52 2
+39 0 15
+`
+
+		mapTests := []struct {
+			source int
+			want   int
+		}{
+			{15, 0},
+			{16, 1},
+			{51, 36},
+			{52, 37},
+			{0, 39},
+		}
+
+		myMap := NewMap(input)
+
+		for _, tt := range mapTests {
+			if myMap[tt.source] != tt.want {
+				t.Errorf("got %v want %v", tt.source, tt.want)
+			}
+		}
+	})
+}
